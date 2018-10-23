d4225ff5327b97ba6d3afba994ed7b450730e2fd
Jeff Gustafson <jgustafson@zynx.com>
2018-10-23 16:27:41 -0700

    Do not use custom AMI feature (since it breaks the output vars.

--- main.tf
+++ main.tf
@@ -7,46 +7,46 @@ data "aws_subnet" "emr_subnet" {
 #
 
 # uses custom AMI
-resource "aws_emr_cluster" "cluster-ami" {
-  count          = "${var.use_custom_ami ? 1 : 0}"
-  name           = "${var.name}"
-  custom_ami_id  = "${var.custom_ami_id}"
-  release_label  = "${var.release_label}"
-  applications   = "${var.applications}"
-  configurations = "${var.configurations}"
-
-  ec2_attributes {
-    key_name                          = "${var.key_name}"
-    subnet_id                         = "${var.subnet_id}"
-    emr_managed_master_security_group = "${aws_security_group.emr_master.id}"
-    emr_managed_slave_security_group  = "${aws_security_group.emr_slave.id}"
-    additional_master_security_groups = "${var.additional_master_security_groups}"
-    service_access_security_group     = "${data.aws_subnet.emr_subnet.map_public_ip_on_launch ? "" : aws_security_group.service_access.id}"
-    instance_profile                  = "${aws_iam_instance_profile.emr_ec2_instance_profile.arn}"
-  }
-
-  instance_group = "${var.instance_groups}"
-
-  autoscaling_role = "${aws_iam_role.emr_autoscaling_role.arn}"
-
-  bootstrap_action {
-    path = "${var.bootstrap_uri}"
-    name = "${var.bootstrap_name}"
-    args = "${var.bootstrap_args}"
-  }
-
-  log_uri      = "${var.log_uri}"
-  service_role = "${aws_iam_role.emr_service_role.arn}"
-
-  tags {
-    Name        = "${var.name}"
-    Project     = "${var.project}"
-    Environment = "${var.environment}"
-  }
-}
+#resource "aws_emr_cluster" "cluster-ami" {
+#  count          = "${var.use_custom_ami ? 1 : 0}"
+#  name           = "${var.name}"
+#  custom_ami_id  = "${var.custom_ami_id}"
+#  release_label  = "${var.release_label}"
+#  applications   = "${var.applications}"
+#  configurations = "${var.configurations}"
+#
+#  ec2_attributes {
+#    key_name                          = "${var.key_name}"
+#    subnet_id                         = "${var.subnet_id}"
+#    emr_managed_master_security_group = "${aws_security_group.emr_master.id}"
+#    emr_managed_slave_security_group  = "${aws_security_group.emr_slave.id}"
+#    additional_master_security_groups = "${var.additional_master_security_groups}"
+#    service_access_security_group     = "${data.aws_subnet.emr_subnet.map_public_ip_on_launch ? "" : aws_security_group.service_access.id}"
+#    instance_profile                  = "${aws_iam_instance_profile.emr_ec2_instance_profile.arn}"
+#  }
+#
+#  instance_group = "${var.instance_groups}"
+#
+#  autoscaling_role = "${aws_iam_role.emr_autoscaling_role.arn}"
+#
+#  bootstrap_action {
+#    path = "${var.bootstrap_uri}"
+#    name = "${var.bootstrap_name}"
+#    args = "${var.bootstrap_args}"
+#  }
+#
+#  log_uri      = "${var.log_uri}"
+#  service_role = "${aws_iam_role.emr_service_role.arn}"
+#
+#  tags {
+#    Name        = "${var.name}"
+#    Project     = "${var.project}"
+#    Environment = "${var.environment}"
+#  }
+#}
 
 # does not use custom AMI
-resource "aws_emr_cluster" "cluster-no-ami" {
+resource "aws_emr_cluster" "cluster" {
   count          = "${var.use_custom_ami ? 0 : 1}"
   name           = "${var.name}"
   release_label  = "${var.release_label}"
